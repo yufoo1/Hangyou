a569ef2cdff25a052d817a092480577593ee363b
周振源 <20373427@buaa.edu.cn>
2022-11-25 01:10:51 +0800

    初步添加follow关系

--- app/src/main/java/com/example/hangyou/ui/home/SearchUserActivity.java
+++ app/src/main/java/com/example/hangyou/ui/home/SearchUserActivity.java
@@ -1,9 +1,13 @@
 package com.example.hangyou.ui.home;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +16,7 @@ import com.example.hangyou.DataBaseHelper;
 import com.example.hangyou.R;
 import com.example.hangyou.ui.group.GroupActivity;
 import com.example.hangyou.ui.group.GroupCardAdapter;
+import com.example.hangyou.ui.group.GroupCardDetailActivity;
 import com.example.hangyou.ui.login.LoginActivity;
 
 import java.util.ArrayList;
@@ -25,8 +30,10 @@ public class SearchUserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_search_user);
         DataBaseHelper helper=new DataBaseHelper(SearchUserActivity.this);
+        initClickListener();
         database = helper.getWritableDatabase();
         database.execSQL("create table if not exists user(id integer primary key autoincrement, account text, password text, username text, description text, phone text)");
+        database.execSQL("create table if not exists follow(id integer primary key autoincrement, followerAccount text, followingAccount text)");
         Cursor cursor = database.rawQuery("select * from user", new String[]{});
         while(cursor.moveToNext()) {
             HashMap<String, Object> item = new HashMap<>();
@@ -41,5 +48,29 @@ public class SearchUserActivity extends AppCompatActivity {
         ListView userCards = findViewById(R.id.search_user_user_list);
         userCards.setAdapter(adapter);
         adapter.notifyDataSetChanged();
+        userCards.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                SharedPreferences sp = getSharedPreferences("login", Context.MODE_PRIVATE);
+                String followerAccount = sp.getString("account", "defaultValue");
+                String followingId = idLists.get(position);
+                Cursor cur = database.rawQuery("select * from user where id=?", new String[]{followingId});
+                cur.moveToFirst();
+                String followingAccount = cur.getString(cur.getColumnIndex("account"));
+                cur.close();
+                database.execSQL("insert into follow(followerAccount, followingAccount) values (?, ?)", new Object[]{followerAccount, followingAccount});
+            }
+        });
+    }
+
+    private void jumpToHome() {
+        Intent intent = new Intent();
+        intent.setClass(this, HomePageActivity.class);
+        startActivity(intent);
+    }
+
+    private void initClickListener() {
+        findViewById(R.id.search_user_return).setOnClickListener(v -> jumpToHome());
     }
 }
