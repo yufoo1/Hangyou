a772a48668ee8d8a7ddce2359f09d9b51ca8cf91
向恩达 <20373560@buaa.edu.cn>
2022-12-27 05:01:14 +0800

    完成树洞基础功能

---
+++ app/src/main/java/com/example/hangyou/ui/tree_hole/PostCardAdapter.java
@@ -0,0 +1,55 @@
+package com.example.hangyou.ui.tree_hole;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.hangyou.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
+
+public class PostCardAdapter extends BaseAdapter {
+    private ArrayList<HashMap<String, Object>> data;
+    private Context ctx;
+
+    public PostCardAdapter(Context ctx, ArrayList<HashMap<String, Object>> data) {
+        this.data = data;
+        this.ctx = ctx;
+    }
+
+    @Override
+    public int getCount() {
+        if (data != null) {
+            return data.size();
+        }
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        convertView = LayoutInflater.from(ctx).inflate(R.layout.post_card, parent, false);
+        TextView tv_postName = convertView.findViewById(R.id.postCardPostName);
+        tv_postName.setText(Objects.requireNonNull(data.get(position).get("postName")).toString());
+        TextView tv_postTime = convertView.findViewById(R.id.postCardPostTime);
+        tv_postTime.setText(Objects.requireNonNull(data.get(position).get("postTime")).toString());
+        TextView tv_postUser = convertView.findViewById(R.id.postCardUserName);
+        tv_postUser.setText(Objects.requireNonNull(data.get(position).get("postUser")).toString());
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/hangyou/ui/tree_hole/TreeHoleActivity.java
+++ app/src/main/java/com/example/hangyou/ui/tree_hole/TreeHoleActivity.java
@@ -1,23 +1,41 @@
 package com.example.hangyou.ui.tree_hole;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.hangyou.DataBaseHelper;
 import com.example.hangyou.MainActivity;
 import com.example.hangyou.R;
 import com.example.hangyou.ui.group.GroupActivity;
+import com.example.hangyou.ui.group.GroupCardAdapter;
+import com.example.hangyou.ui.group.GroupCardDetailActivity;
+import com.example.hangyou.ui.group.GuideActivity;
 import com.example.hangyou.ui.home.HomePageActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
 
 public class TreeHoleActivity extends AppCompatActivity {
     SQLiteDatabase database;
+    ArrayList<HashMap<String, Object>> data;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_tree_hole);
         RadioGroup mRadioGroup=findViewById(R.id.tree_hole_tabs);
+        initClickListener();
+        DataBaseHelper helper = new DataBaseHelper(TreeHoleActivity.this);
+        database = helper.getWritableDatabase();
+        showPostCards();
         mRadioGroup.setOnCheckedChangeListener((radioGroup, i) -> {
             Intent intent = new Intent();
             switch (i) {
@@ -34,4 +52,60 @@ public class TreeHoleActivity extends AppCompatActivity {
             startActivity(intent);
         });
     }
+
+
+    private void initClickListener() {
+        findViewById(R.id.click_begin_post).setOnClickListener(v -> jumpToCreate());
+    }
+
+    private void jumpToCreate() {
+        Intent intent = new Intent();
+        intent.setClass(TreeHoleActivity.this, TreeHolePostCreate.class);
+        startActivity(intent);
+    }
+
+    private void showPostCards() {
+        data = new ArrayList<>();
+        Cursor cursor = database.rawQuery("select * from post", new String[]{});
+        ArrayList<Integer> idLists = new ArrayList<>();
+        HashMap<String, Object> item;
+        while(cursor.moveToNext()) {
+            item = new HashMap<>();
+            item.put("postName", cursor.getString(cursor.getColumnIndex("postName")));
+            item.put("postTime", cursor.getString(cursor.getColumnIndex("createTime")));
+            int userId = Integer.parseInt(cursor.getString(cursor.getColumnIndex("userId")));
+            Cursor cursor2 = database.rawQuery("select * from user where id=?", new String[]{String.valueOf(userId)});
+            cursor2.moveToFirst();
+            item.put("postUser", cursor2.getString(cursor2.getColumnIndex("username")));
+            /*while(cursor2.moveToNext()) {
+                System.out.println(cursor2.getString(cursor2.getColumnIndex("id")));
+                System.out.println(userId);
+                if(Objects.equals(cursor2.getString(cursor2.getColumnIndex("id")), String.valueOf(userId))){
+                    item.put("postUser", cursor2.getString(cursor2.getColumnIndex("username")));
+                    break;
+                }
+            }*/
+            cursor2.close();
+            item.put("id", cursor.getString(cursor.getColumnIndex("id")));
+            idLists.add(Integer.parseInt(cursor.getString(cursor.getColumnIndex("id"))));
+            data.add(item);
+        }
+        cursor.close();
+        PostCardAdapter adapter = new PostCardAdapter(TreeHoleActivity.this, data);
+        ListView postCards = findViewById(R.id.post_cards);
+        postCards.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+        postCards.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Bundle bundle=new Bundle();
+                bundle.putInt("id", idLists.get(position));
+                Intent intent =new Intent(TreeHoleActivity.this, TreeHolePostView.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
 }
