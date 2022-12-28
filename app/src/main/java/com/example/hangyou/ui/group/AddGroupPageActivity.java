2d2c7e52dc1e469e0c5f4081847c0ec5d22560d4
周振源 <20373427@buaa.edu.cn>
2022-12-28 23:55:18 +0800

    添加组局页mysql迁移

--- app/src/main/java/com/example/hangyou/ui/group/AddGroupPageActivity.java
+++ app/src/main/java/com/example/hangyou/ui/group/AddGroupPageActivity.java
@@ -3,8 +3,6 @@ package com.example.hangyou.ui.group;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -12,21 +10,22 @@ import android.widget.RadioButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.hangyou.utils.DataBaseHelper;
 import com.example.hangyou.R;
+import com.example.hangyou.utils.MysqlConnector;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class AddGroupPageActivity extends AppCompatActivity {
-    SQLiteDatabase database;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_add_group_page);
-        DataBaseHelper helper = new DataBaseHelper(AddGroupPageActivity.this);
-        database = helper.getWritableDatabase();
-        database.execSQL("create table if not exists user_group(id integer primary key autoincrement, groupName text, groupType text, groupInitiator text, groupDescription text, groupYear int, groupMonth int, groupDay int, groupMaleExpectedNum int, groupMaleNowNum int, groupFemaleExpectedNum int, groupFemaleNowNum int)");
-        database.execSQL("create table if not exists user_group_relation(id integer primary key autoincrement, userId integer, groupId integer)");
         initClickListener();
     }
 
@@ -39,7 +38,13 @@ public class AddGroupPageActivity extends AppCompatActivity {
         RadioButton rb_type5 = findViewById(R.id.add_group_page_type_5);
         RadioButton rb_type6 = findViewById(R.id.add_group_page_type_6);
         RadioButton rb_type7 = findViewById(R.id.add_group_page_type_7);
-        findViewById(R.id.add_group_page_button_created).setOnClickListener(v -> commit());
+        findViewById(R.id.add_group_page_button_created).setOnClickListener(v -> {
+            try {
+                commit();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
         findViewById(R.id.add_group_page_button_return).setOnClickListener(v -> returnToGroup());
         findViewById(R.id.add_group_page_type_0).setOnClickListener(v -> {
             rb_type1.setChecked(false);
@@ -121,7 +126,7 @@ public class AddGroupPageActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private void commit() {
+    private void commit() throws SQLException {
         EditText et_name = findViewById(R.id.add_group_page_name_input);
         String groupName = et_name.getText().toString();
         RadioButton rb_type0 = findViewById(R.id.add_group_page_type_0);
@@ -164,31 +169,148 @@ public class AddGroupPageActivity extends AppCompatActivity {
         SharedPreferences sp = getSharedPreferences("login", Context.MODE_PRIVATE);
         String account = sp.getString("account", "defaultValue");
         System.out.println(account);
-        Cursor cursor = database.rawQuery("select * from user where account=?", new String[]{account});
-        cursor.moveToFirst();
-        String groupInitiator = cursor.getString(cursor.getColumnIndex("username"));
-        String id = cursor.getString(cursor.getColumnIndex("id"));
-        String gender = cursor.getString(cursor.getColumnIndex("gender"));
-        cursor.close();
-        database.execSQL("insert into user_group(groupName, groupType, groupInitiator, groupDescription, groupYear, groupMonth, groupDay, groupMaleExpectedNum, groupMaleNowNum, groupFemaleExpectedNum, groupFemaleNowNum) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", new Object[]{groupName, groupType, groupInitiator, groupDescription, groupYear, groupMonth, groupDay, groupMaleExpectedNum, 0, groupFemaleExpectedNum, 0});
-        cursor = database.rawQuery("select Max(id) as maxId from user_group", new String[]{});
-        cursor.moveToFirst();
-        String groupId = cursor.getString(cursor.getColumnIndex("maxId"));
-        database.execSQL("insert into user_group_relation(groupId, userId) values(?, ?)", new String[]{String.valueOf(groupId), String.valueOf(id)});
+        AtomicBoolean flag1 = new AtomicBoolean(false);
+        AtomicReference<ResultSet> resultSet = new AtomicReference<>();
+        new Thread(() -> {
+            try {
+                Connection connection = MysqlConnector.getConnection();
+                String sql = "select * from user where account=?";
+                PreparedStatement ps = connection.prepareStatement(sql);
+                ps.setString(1, account);
+                resultSet.set(ps.executeQuery());
+                flag1.set(true);
+            } catch (InterruptedException | SQLException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        while(!flag1.get());
+        resultSet.get().next();
+        String groupInitiator = resultSet.get().getString("username");
+        String id = resultSet.get().getString("id");
+        String gender = resultSet.get().getString("gender");
+        flag1.set(false);
+        new Thread(() -> {
+            try {
+                Connection connection = MysqlConnector.getConnection();
+                String sql = "insert into user_group(groupName, groupType, groupInitiator, description, year, month, day, maleExpect, maleNow, femaleExpect, femaleNow) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                PreparedStatement ps = connection.prepareStatement(sql);
+                ps.setString(1, groupName);
+                ps.setString(2, groupType);
+                ps.setString(3, groupInitiator);
+                ps.setString(4, groupDescription);
+                ps.setString(5, String.valueOf(groupYear));
+                ps.setString(6, String.valueOf(groupMonth));
+                ps.setString(7, String.valueOf(groupDay));
+                ps.setString(8, String.valueOf(groupMaleExpectedNum));
+                ps.setString(9, String.valueOf(0));
+                ps.setString(10, String.valueOf(groupFemaleExpectedNum));
+                ps.setString(11, String.valueOf(0));
+                ps.executeUpdate();
+                flag1.set(true);
+            } catch (InterruptedException | SQLException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        while (!flag1.get());
+        flag1.set(false);
+        new Thread(() -> {
+            try {
+                Connection connection = MysqlConnector.getConnection();
+                String sql = "select id from user_group order by id desc";
+                PreparedStatement ps = connection.prepareStatement(sql);
+                resultSet.set(ps.executeQuery());
+                flag1.set(true);
+            } catch (InterruptedException | SQLException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        while (!flag1.get())
+        resultSet.get().next();
+        String groupId = resultSet.get().getString("id");
+        flag1.set(false);
+        new Thread(() -> {
+            try {
+                Connection connection = MysqlConnector.getConnection();
+                String sql = "insert into user_group_relation(groupId, userId) values(?, ?)";
+                PreparedStatement ps = connection.prepareStatement(sql);
+                ps.setString(1, String.valueOf(groupId));
+                ps.setString(2, String.valueOf(id));
+                ps.executeUpdate();
+                flag1.set(true);
+            } catch (InterruptedException | SQLException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        resultSet.get().next();
         if(gender.equals("男")) {
-            cursor = database.rawQuery("select * from user_group, user, user_group_relation where user_group.id=user_group_relation.groupId and user.id=user_group_relation.userId and gender='男' and user_group.id=?", new String[]{String.valueOf(groupId)});
+            flag1.set(false);
+            new Thread(() -> {
+                try {
+                    Connection connection = MysqlConnector.getConnection();
+                    String sql = "select * from user_group, user, user_group_relation where user_group.id=user_group_relation.groupId and user.id=user_group_relation.userId and gender='男' and user_group.id=?";
+                    PreparedStatement ps = connection.prepareStatement(sql);
+                    ps.setString(1, String.valueOf(groupId));
+                    resultSet.set(ps.executeQuery());
+                    flag1.set(true);
+                } catch (InterruptedException | SQLException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+            while (!flag1.get());
             int cnt = 0;
-            while(cursor.moveToNext()) {
+            while(resultSet.get().next()) {
                 cnt++;
             }
-            database.execSQL("update user_group set groupMaleNowNum=? where id=?", new String[]{String.valueOf(cnt), String.valueOf(groupId)});
+            flag1.set(false);
+            int finalCnt = cnt;
+            new Thread(() -> {
+                try {
+                    Connection connection = MysqlConnector.getConnection();
+                    String sql = "update user_group set maleNow=? where id=?";
+                    PreparedStatement ps = connection.prepareStatement(sql);
+                    ps.setString(1, String.valueOf(finalCnt));
+                    ps.setString(2, String.valueOf(String.valueOf(groupId)));
+                    ps.executeUpdate();
+                    flag1.set(true);
+                } catch (InterruptedException | SQLException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+            while (!flag1.get());
         } else {
-            cursor = database.rawQuery("select * from user_group, user, user_group_relation where user_group.id=user_group_relation.groupId and user.id=user_group_relation.userId and gender='女' and user_group.id=?", new String[]{String.valueOf(groupId)});
+            flag1.set(false);
+            new Thread(() -> {
+                try {
+                    Connection connection = MysqlConnector.getConnection();
+                    String sql = "select * from user_group, user, user_group_relation where user_group.id=user_group_relation.groupId and user.id=user_group_relation.userId and gender='女' and user_group.id=?";
+                    PreparedStatement ps = connection.prepareStatement(sql);
+                    ps.setString(1, String.valueOf(groupId));
+                    resultSet.set(ps.executeQuery());
+                    flag1.set(true);
+                } catch (InterruptedException | SQLException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+            while (!flag1.get());
             int cnt = 0;
-            while(cursor.moveToNext()) {
+            while(resultSet.get().next()) {
                 cnt++;
             }
-            database.execSQL("update user_group set groupFemaleNowNum=? where id=?", new String[]{String.valueOf(cnt), String.valueOf(groupId)});
+            flag1.set(false);
+            int finalCnt = cnt;
+            new Thread(() -> {
+                try {
+                    Connection connection = MysqlConnector.getConnection();
+                    String sql = "update user_group set groupFemaleNowNum=? where id=?";
+                    PreparedStatement ps = connection.prepareStatement(sql);
+                    ps.setString(1, String.valueOf(finalCnt));
+                    ps.setString(2, String.valueOf(groupId));
+                    ps.executeUpdate();
+                    flag1.set(true);
+                } catch (InterruptedException | SQLException e) {
+                    e.printStackTrace();
+                }
+            }).start();
         }
         System.out.println("新组添加成功");
         Intent intent = new Intent();
