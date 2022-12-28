dbeac056c14e2af5622f4d81124be0c6a6b8f878
周振源 <20373427@buaa.edu.cn>
2022-12-29 07:21:27 +0800

    调整主页布局

--- app/src/main/java/com/example/hangyou/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/hangyou/ui/login/LoginActivity.java
@@ -3,8 +3,6 @@ package com.example.hangyou.ui.login;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,7 +10,6 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.hangyou.utils.DataBaseHelper;
 import com.example.hangyou.R;
 import com.example.hangyou.ui.group.GroupActivity;
 import com.example.hangyou.utils.MysqlConnector;
@@ -22,10 +19,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 
 public class LoginActivity extends AppCompatActivity{
-    ResultSet rs = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +31,7 @@ public class LoginActivity extends AppCompatActivity{
     }
 
     public void login() throws SQLException {
+        AtomicReference<ResultSet> rs = new AtomicReference<>();
         EditText et_account = findViewById(R.id.account);
         String account = et_account.getText().toString();
         EditText et_password = findViewById(R.id.password);
@@ -51,20 +49,20 @@ public class LoginActivity extends AppCompatActivity{
                     PreparedStatement ps = connection.prepareStatement(sql);
                     ps.setString(1, account);
                     ps.setString(2, password);
-                    rs = ps.executeQuery();
+                    rs.set(ps.executeQuery());
                     flag1.set(true);
                 } catch (InterruptedException | SQLException e) {
                     e.printStackTrace();
                 }
             }).start();
             while(!flag1.get());
-            if (rs.next()) {
+            if (rs.get().next()) {
                 System.out.println("登录成功");
                 Toast.makeText(this, "登录成功", Toast.LENGTH_SHORT).show();
                 SharedPreferences sp = getSharedPreferences("login", Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = sp.edit();
                 editor.putString("account", account);
-                editor.putString("head_portrait", rs.getString("headPortrait"));
+                editor.putString("head_portrait", rs.get().getString("headPortrait"));
                 editor.apply();
                 Intent intent = new Intent();
                 intent.setClass(LoginActivity.this, GroupActivity.class);
@@ -77,7 +75,7 @@ public class LoginActivity extends AppCompatActivity{
                         String sql = "select * from user where account=?";
                         PreparedStatement ps = connection.prepareStatement(sql);
                         ps.setString(1, account);
-                        rs = ps.executeQuery();
+                        rs.set(ps.executeQuery());
                         flag2.set(true);
                     } catch (InterruptedException | SQLException e) {
                         e.printStackTrace();
