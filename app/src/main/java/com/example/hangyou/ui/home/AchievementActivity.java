9cb3f74eb827c5d0a0298d96b9ddeedf735f7217
裴子祎 <20373780@buaa.edu.cn>
2022-12-29 15:55:22 +0800

    achievment_page

---
+++ app/src/main/java/com/example/hangyou/ui/home/AchievementActivity.java
@@ -0,0 +1,21 @@
+package com.example.hangyou.ui.home;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hangyou.R;
+
+
+public class AchievementActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_achievement);
+    }
+}
